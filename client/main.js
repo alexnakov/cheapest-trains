6408fbfa0ebdcce5a4d6788cbe648ba2438d70f3
alexnakov <alex.nakov3@gmail.com>
2024-07-20 13:23:22 +0100

    getting current month

---
+++ client/main.js
@@ -0,0 +1,6 @@
+const monthsArray = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+const now = new Date()
+const currentMonth = now.getMonth()
+
+
