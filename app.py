db2073289d430d15e33b8cbd475a9014e89d164e
alexnakov <alex.nakov3@gmail.com>
2024-07-24 09:53:59 +0100

    Added Firedoc class to app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, url_for
 from read_firebase import get_all_data
+import pickle
+
+class FirebaseDoc():
+    def __init__(self, _data):
+        self._data = _data
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Data from firebase line below
-    # data = get_all_data() 
+    with 
 
     return render_template('index.html')
 
